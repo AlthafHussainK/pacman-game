78c8e6b79cb9c0f088791b71a6927d9b9cdb87d5
AlthafHussainK <althafakm@gmail.com>
2021-03-21 12:41:57 +0530

    make ghosts

--- js/index.js
+++ js/index.js
@@ -130,3 +130,17 @@ function pacDotEaten(){
   }
 }
 
+class Ghost {
+  constructor(className, startIndex, speed){
+    this.className = className
+    this.startIndex = startIndex
+    this.speed = speed
+  }
+}
+
+ghosts = [
+  new Ghost('blinky', 348, 250),
+  new Ghost('inky', 351, 300),
+  new Ghost('pinky', 376, 400),
+  new Ghost('clyde', 379, 500)
+]
